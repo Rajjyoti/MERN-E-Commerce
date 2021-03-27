107043ea647911d1d215c2e55cdc2331a8151376
Rajjyoti <rajjyotidoley1@gmail.com>
2021-03-28 01:23:51 +0530

    React sec

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 /*You can have only one element inside a function, here its a header*/
 const App = () => {
   return (
     <>
+    <Header />
+    <main className='py-3'>
+      <Container>
       <h1>Welcome To Proshop</h1>
+      </Container>
+      
+    </main>
+    <Footer />
+      
     </>
   );
 }
