5f06da93fc854e6db4b45472bbf8d6c314b6e61b
Rajjyoti <rajjyotidoley1@gmail.com>
2021-03-28 00:23:04 +0530

    React setup

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+/*You can have only one element inside a function, here its a header*/
+const App = () => {
+  return (
+    <>
+      <h1>Welcome To Proshop</h1>
+    </>
+  );
+}
+
+export default App;
