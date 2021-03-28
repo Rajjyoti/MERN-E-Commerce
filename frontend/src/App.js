5ca6cdf6b4b0cf08a1d8772a144a3f8189a86ff3
Rajjyoti <rajjyotidoley1@gmail.com>
2021-03-28 17:43:48 +0530

    next

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
 /*You can have only one element inside a function, here its a header*/
 const App = () => {
@@ -10,7 +11,7 @@ const App = () => {
     <Header />
     <main className='py-3'>
       <Container>
-      <h1>Welcome To Proshop</h1>
+      <HomeScreen />
       </Container>
       
     </main>
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
             <Container>
                 <Row>
                     <Col className='text-center py-3'>
-                        Copyright &copy; Proshop
+                        Copyright &copy; Shopvilla
                     </Col>
                 </Row>
             </Container>
